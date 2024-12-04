fd544be9e6dddf68e1d965259d5b6f08f0f26c67
Loralyn Milcarek <98704514+loralyn-milcarek@users.noreply.github.com>
2024-12-04 18:40:22 +0000

    update file writer with code to read input file

--- file-writer.ts
+++ file-writer.ts
@@ -5,14 +5,17 @@ const writeCurrentDayFiles = () => {
 
   const fileDate = today < 10 ? `0${today}` : today.toString();
 
+  const inputFileName = `input/${fileDate}.txt`;
+
   fs.writeFileSync(
     `puzzles/${fileDate}.ts`,
     `import { readFileSync } from 'fs';
 
+    const fileContent = fs.readFileSync(inputFileName, 'utf-8');
 `
   );
 
-  fs.writeFileSync(`input/${fileDate}.txt`, '');
+  fs.writeFileSync(inputFileName, '');
 };
 
 writeCurrentDayFiles();
