4096f428be78ab634631c8f937006a82a3f87185
Loralyn Milcarek <98704514+loralyn-milcarek@users.noreply.github.com>
2024-12-10 19:36:57 +0000

    day 10 part 1

--- puzzles/10.ts
+++ puzzles/10.ts
@@ -28,7 +28,7 @@ const countNineCache = () => {
 };
 
 const isNextValueEast = (current: number, x: number, y: number) => {
-    if (grid[x][y + 1] === 9) {
+    if (grid[x][y + 1] === 9 && current === 8) {
         addNineToCache(x, y + 1);
         return false;
     }
@@ -36,7 +36,7 @@ const isNextValueEast = (current: number, x: number, y: number) => {
 }
 
 const isNextValueSouth = (current: number, x: number, y: number) => {
-    if (grid[x + 1] && grid[x + 1][y] === 9) {
+    if (grid[x + 1] && grid[x + 1][y] === 9 && current === 8) {
         addNineToCache(x + 1, y);
         return false;
     }
@@ -45,7 +45,7 @@ const isNextValueSouth = (current: number, x: number, y: number) => {
 }
 
 const isNextValueWest = (current: number, x: number, y: number) => {
-    if (grid[x][y - 1] === 9) {
+    if (grid[x][y - 1] === 9 && current === 8) {
         addNineToCache(x, y - 1);
         return false;
     }
@@ -53,7 +53,7 @@ const isNextValueWest = (current: number, x: number, y: number) => {
 }
 
 const isNextValueNorth = (current: number, x: number, y: number) => {
-    if (grid[x - 1] && grid[x - 1][y] === 9) {
+    if (grid[x - 1] && grid[x - 1][y] === 9 && current === 8) {
         addNineToCache(x - 1, y);
         return false;
     }
@@ -61,14 +61,14 @@ const isNextValueNorth = (current: number, x: number, y: number) => {
         && grid[x - 1][y] === current + 1;
 }
 
-function findNextValue(current: number, x: number, y: number) {
+const findNextValue = (current: number, x: number, y: number) => {
     if (isNextValueEast(current, x, y)) findNextValue(current + 1, x, y + 1);
     if (isNextValueSouth(current, x, y)) findNextValue(current + 1, x + 1, y);
     if (isNextValueWest(current, x, y)) findNextValue(current + 1, x, y - 1);
     if (isNextValueNorth(current, x, y)) findNextValue(current + 1, x - 1, y);
 }
 
-function traverseMap() {
+const traverseMap = () => {
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === 0) {
@@ -82,50 +82,8 @@ function traverseMap() {
 }
 
 
-// console.log(traverseMap());
-// trailheadScores;
-// grid;
+console.log(traverseMap()); // 36, 737
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// function position(value: number, x: number, y: number) {
-//     this.value = value;
-//     this.x = x;
-//     this.y = y;
-//     this.ninesReached = 0;
-//     this.moveEast = (value: number, x: number, y: number) => {
-//         const next = grid[x][y + 1];
-//         // if num is one larger than value
-//         if (value + 1 === next) {
-//             // if num is 9
-//             if (next === 9) {
-//                 // overwrite 9, return 1
-//                 grid[x][y + 1] = -1;
-//                 return 1;
-//             }
-//             // call moveEast
-//             return this.moveEast(next, x, y + 1);
-//             // call moveSouth
-//             return this.moveSouth(next, x, y + 1);
-//             // call moveWest
-
-//             // call moveNorth
-//         }
-//         // return 0;
-//     };
-//     this.moveSouth = () => {};
-//     this.moveWest = () => {};
-//     this.moveNorth = () => {};
-// }
